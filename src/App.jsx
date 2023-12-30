3af7a56332113b096b332a30a99d0a0ffcd1f4d0
igor3286411 <igor214180@gmail.com>
2023-12-30 17:20:01 +0100

    Start lesson 5

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import UserData from "./componets/UserData";
+import LoadingPersonsData from "./componets/LoadingPersonsData";
+// import "./App.css";
+// import { Table } from "./componets/Table/Table";
+// import tableData1 from "./tableData.json"
+
+// const columns = [
+//   { lable: "Full Name", key: "full_name", sortable: true },
+//   { lable: "Gender", key: "gender", sortable: true, sortbyOrder: 'desc' },
+//   { lable: "Age", key: "age", sortable: true },
+//   { lable: "Email", key: "email", sortable: false },
+// ];
+
+// return (
+//   <div>
+//     <h1>Sortable table</h1>
+//     <Table columns={columns} data={tableData1}/>
+//   </div>
+// );
+
+function App() {
+  const DataLoading = LoadingPersonsData(UserData);
+
+  const [appState, setAppState] = useState({
+    loading: false,
+    persons: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl =
+      "https://jsonplaceholder.typicode.com/users";
+    axios.get(apiUrl).then((resp) => {
+      const allPersons = resp.data;
+      setAppState({
+        loading: false,
+        persons: allPersons,
+      });
+    });
+  }, [setAppState]);
+
+  return (
+    <div className="app">
+      <DataLoading isLoading={appState.loading} persons={appState.persons} />
+    </div>
+  );
+}
+
+export default App;
